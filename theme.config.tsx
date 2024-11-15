e084b47f6db0fb1d3b872e040719296943b4c3fd
Lee <lee@mtftm.com>
2024-11-15 21:44:32 +0800

    feat: add auto title and Open Graph support

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
-
-const config: DocsThemeConfig = {
-  logo: <span>中文跨性别维基</span>
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
+ 
+export default {
+  logo: <b>中文跨性别维基</b>,
+  head() {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://my-app.com' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <title>{frontMatter.title ? `${frontMatter.title} - 中文跨性别维基` : '中文跨性别维基'}</title>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || '中文跨性别维基'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || '这里写摘要，先占个位置'}
+        />
+      </>
+    )
+  }
 }
-
-export default config
