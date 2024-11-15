3f1f02c13585b2ab11c9edea9db3639089d7df46
千坂神奈子 <chisakakanako@gmail.com>
2024-11-15 20:03:26 +0800

    更新网站模板

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+  logo: <span>中文跨性别维基</span>
 }
 
 export default config
